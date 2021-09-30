f5b36e2be2731f2baafb7f6758ea1a1092ec1214
chrispy <jones_christopher_b@protonmail.com>
2021-09-30 17:48:12 -0400

    testing code fastpwmtmr massive updates

--- fastpwmtmr/fastpwmtmr.ino
+++ fastpwmtmr/fastpwmtmr.ino
@@ -1,25 +1,43 @@
+#define _RAMP_
+//#define _DIVTWO_  // 31KHz
+#define _DIVFOUR_  // 3.9KHz
+
+
 #define PWM_PIN 11
 #define ADC_PIN A0
 
-#define INC_DEL 50  //Delay time between incremet/decrement of dutyCycle
-#define NUM_INC 127 //Num times tmr ovf isr happens before increment of dutyCycle
 
-uint8_t dutyCycle = 0; // 0-255 for 0%-100%
-uint8_t incFlag = 0;
+//#define INC_DEL 50  //Delay time between incremet/decrement of dutyCycle
+
+#ifdef _DIVTWO_
+#define NUM_INC 127 //Num times tmr ovf isr happens before increment of dutyCycle
+//clipping occurs at dutyCycle values less than DTY_MIN, greater than DTY_MAX 
+#define DTY_MIN 13 //dutyCycle minimum value
+#define DTY_MAX 238 //dutyCycle maximum value
+#define PRESCLR 0b00000001
+#endif
+
+#ifdef _DIVFOUR_
+#define NUM_INC 15 //Num times tmr ovf isr happens before increment of dutyCycle
+//clipping occurs at dutyCycle values less than DTY_MIN, greater than DTY_MAX 
+#define DTY_MIN 17 //dutyCycle minimum value
+#define DTY_MAX 240 //dutyCycle maximum value
+#define PRESCLR 0b00000010
+#endif
+
+uint8_t dutyCycle = 127; // 0-255 for 0%-100%
+uint16_t incFlag = 0;
 uint8_t updn = 0;
 
 void setup() {
   TIMSK2 = (TIMSK2 & 0b11111000) | 0b00000001; // use TIMER2_OVF_vect
   
 //  BEGIN   PWM SETUP
-  TCCR2B = TCCR2B & 0b11111000 | 0b00000010; // 3.9KHz
-//  TCCR2B = TCCR2B & 0b11111000 | 0b00000001; // 31KHz
+  TCCR2B = TCCR2B & 0b11111000 | PRESCLR;
   pinMode(PWM_PIN, OUTPUT);
   analogWrite(PWM_PIN, dutyCycle);
 //  END     PWM SETUP
 
-  //OCR2A = 254; // compare value A
-  //OCR2B = 255; // compare value B
 
 //  BEGIN   ADC SETUP
   pinMode(ADC_PIN, INPUT);
@@ -30,30 +48,14 @@ void setup() {
 }
 
 void loop() {
-//  Serial.println("enterloop");
-//  while(dutyCycle < 255){
-//    if(incFlag >= NUM_INC){
-//      incFlag = 0;
-//      Serial.println(dutyCycle);
-//      analogWrite(PWM_PIN, dutyCycle++);
-//    }
-//    delay(1);
-//  }
-//  while(dutyCycle > 0){
-//    if(incFlag >= NUM_INC){
-//      incFlag = 0;
-//      Serial.println(dutyCycle);
-//      analogWrite(PWM_PIN, dutyCycle--);
-//    }
-//    delay(1);
-//  }
+  
 
-
+#ifdef _RAMP_
     if(incFlag >= NUM_INC & updn == 0){
       incFlag = 0;
       dutyCycle++;
       analogWrite(PWM_PIN, dutyCycle);
-      if(dutyCycle == 255){
+      if(dutyCycle == DTY_MAX){
         updn = 1;
       }
 
@@ -62,27 +64,19 @@ void loop() {
       incFlag = 0;
       dutyCycle--;
       analogWrite(PWM_PIN, dutyCycle);
-      if(dutyCycle == 0){
+      if(dutyCycle == DTY_MIN){
         updn = 0;
       }
     }
-
+#else
+    delay(10);
+#endif
     
     Serial.print(dutyCycle);
     Serial.print(", ");
     Serial.println(analogRead(ADC_PIN));
 }
 
-
-//when TCNT2 == OCR2A
-//ISR(TIMER2_COMPA_vect){
-//  Serial.println(analogRead(ADC_PIN));
-//}
-
-// //when TCNT2 == OCR2B
-//ISR(TIMER2_COMPB_vect){
-//  incFlag++;
-//}
 
 ISR(TIMER2_OVF_vect){
   incFlag++;
