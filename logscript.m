4363b6a9846fa42ba5a65e6f47a2e20f5de21417
Chrispy <jones_christopher_b@protonmail.com>
2021-09-30 20:14:36 -0400

    added rough oscope measurement plotting script, updated fileinfo.csv

---
+++ logscript.m
@@ -0,0 +1,34 @@
+clc
+clear
+close all
+
+path = "./oscope_logs/";
+infopath = path + "fileinfo.csv";
+
+fileinfo = readtable(infopath,'Delimiter', ',');
+numLogs = height(fileinfo);
+
+for n = 1:numLogs
+    filepath = path + string(fileinfo.FileName(n));
+    BK = readtable(filepath, 'Delimiter', ',');
+    
+    plotTitle = string(fileinfo.FileName(n)) + " (" + string(fileinfo.R_test(n)) + ", " + string(fileinfo.f_pwm(n)) + ")";
+    plotyLabel = string(fileinfo.CH1(n)) + ", "+ string(fileinfo.CH2(n))+ " (V)";
+    BKt = BK.Var4(3:end);
+    BKCH1 = BK.Var5(3:end);
+    BKCH2 = BK.Var6(3:end);
+    
+    ch1ts = timeseries(BKCH1, BKt);
+    ch2ts = timeseries(BKCH2, BKt);
+
+    figure(n)
+    hold on
+    plot(ch1ts);
+    plot(ch2ts);
+
+    title(plotTitle);
+    ylabel(plotyLabel);
+    xlabel('Time');
+    hold off
+    
+end
